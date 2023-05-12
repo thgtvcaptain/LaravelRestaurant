a616bee9c98f7aa1cf9f6c438d0d91e9db47d965
thgtvcaptain <107764455+thgtvcaptain@users.noreply.github.com>
2023-05-12 22:06:53 +0700

    Setup Admin Middleware
    Update Admin Controller
    Setup Admin View

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -16,7 +16,7 @@ class Admin
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if(!Auth::check() || Auth::user()->is_admin) {
+        if(!Auth::check() || !Auth::user()->is_admin) {
             abort(403);
         }
         return $next($request);
